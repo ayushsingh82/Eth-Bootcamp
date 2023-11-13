2230c99cd40597fb9de72127199ccc890ae31c90
Ayush Singh <121667116+ayushsingh82@users.noreply.github.com>
2023-11-13 15:14:05 +0530

    Add files via upload
    week2 done

---
+++ Week2/GiftList/client/index.js
@@ -0,0 +1,23 @@
+const axios = require('axios');
+const niceList = require('../utils/niceList.json');
+const MerkleTree = require('../utils/MerkleTree');
+
+const serverUrl = 'http://localhost:1225';
+
+async function main() {
+   
+ const name=process.argv[2] || "Ayush"
+  console.log(name)
+
+  const merkleTree=new MerkleTree(niceList)
+  const index=niceList.findIndex(n=>n===name)
+  const proof=merkleTree.getProof(index)
+   
+  const { data: gift } = await axios.post(`${serverUrl}/gift`, {
+     name,proof
+  });
+
+  console.log({ gift });
+}
+
+main();
